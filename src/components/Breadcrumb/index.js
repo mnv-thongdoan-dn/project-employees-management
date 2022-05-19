76dc8308c806d795ec80066ce7e3fd09016dd254
Thong Doan <thong.doan@monstar-lab.com>
2022-05-19 08:04:47 +0700

    handle Breadcrum

--- src/components/Breadcrumb/index.js
+++ src/components/Breadcrumb/index.js
@@ -1,14 +1,40 @@
-import React from 'react';
-import { Breadcrumb } from 'antd';
+import React from "react";
+import { useLocation, Link } from "react-router-dom";
+import { Breadcrumb } from "antd";
 
-const BreadcrumbDashboard = () => {
-  return (
-  <Breadcrumb>
-    <Breadcrumb.Item>Home</Breadcrumb.Item>
-    <Breadcrumb.Item>List</Breadcrumb.Item>
-    <Breadcrumb.Item>App</Breadcrumb.Item>
-  </Breadcrumb>
-  )
-}
+const BreadCrumb = () => {
+  const location = useLocation();
+  const breadCrumbView = () => {
+    const { pathname } = location;
+    const pathnames = pathname.split("/").filter((item) => item);
+    const capatilize = (s) => s.charAt(0).toUpperCase() + s.slice(1);
+    return (
+      <div>
+        <Breadcrumb>
+          {/* {pathnames.length > 0 ? (
+            <Breadcrumb.Item>
+              <Link to="/Dashboard">Dashboard</Link>
+            </Breadcrumb.Item>
+          ) : (
+            <Breadcrumb.Item>Dashboard</Breadcrumb.Item>
+          )} */}
+          {pathnames.map((name, index) => {
+            const routeTo = `/${pathnames.slice(0, index + 1).join("/")}`;
+            const isLast = index === pathnames.length - 1;
+            return isLast ? (
+              <Breadcrumb.Item key={index}>{capatilize(name)}</Breadcrumb.Item>
+            ) : (
+              <Breadcrumb.Item key={index}>
+                <Link to={`${routeTo}`}>{capatilize(name)}</Link>
+              </Breadcrumb.Item>
+            );
+          })}
+        </Breadcrumb>
+      </div>
+    );
+  };
 
-export default BreadcrumbDashboard;
+  return <>{breadCrumbView()}</>;
+};
+
+export default BreadCrumb;
